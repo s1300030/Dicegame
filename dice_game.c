973b2c0165b6f2254911ecf1a4912586e612fa9f
KUWANA Kohei <s1300030@u-aizu.ac.jp>
2023-06-09 12:44:53 +0900

    Won/lost message modified

--- dice_game.c
+++ dice_game.c
@@ -21,8 +21,8 @@ int main(void){
   printf("Total value: %d\n",a+b);
 
   if(a+b>7){
-    printf("You won\n");
-  }else printf("You lost\n");
+    printf("%s won\n",name);
+  }else printf("%s lost\n",name);
 
   return 0;
 
